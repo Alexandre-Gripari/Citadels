7bb711b2b8f9102df5cc8c363bcc3106591210c2
YohanSI3 <yohan.mazzi@etu.unice.fr>
2023-12-04 14:26:28 +0100

    Classe Player avec stratégie de base #2

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/Player.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/Player.java
@@ -1,11 +1,80 @@
 package fr.cotedazur.univ.polytech.startingpoint;
 
 
+import fr.cotedazur.univ.polytech.startingpoint.cards.Constructions;
 import fr.cotedazur.univ.polytech.startingpoint.players.City;
 import fr.cotedazur.univ.polytech.startingpoint.players.Hand;
 
 public class Player {
-    private int or;
+    private int number;
+    private int gold;
     private Hand hand;
     private City city;
+
+    public Player(int number, Hand hand){
+        this(number, 2, hand, new City());
+    }
+
+    /* Constructeur utile aux tests */
+    public Player(int number, int gold, Hand hand, City city){
+        this.number = number;
+        this.gold = gold;
+        this.hand = hand;
+        this.city = city;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public int getGold() {
+        return gold;
+    }
+
+    public Hand getHand() {
+        return hand;
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    public void play(Draw draw){
+        if (hand.isEmpty()) hand.add(takeConstruction(draw));
+        else takeGold();
+        buildConstruction();
+    }
+
+    /* Renvoie la construction (quartier) la moins chère entre les 2 en haut de la pioche */
+    public Constructions takeConstruction(Draw d){
+        Constructions c1 = d.draw();
+        Constructions c2 = d.draw();
+        if (c1.getValue() <= c2.getValue()){
+            d.add(c2);
+            return c1;
+        }
+        else {
+            d.add(c1);
+            return c2;
+        }
+    }
+
+    public void takeGold(){ gold += 2; }
+
+    public void buildConstruction(){
+        for (int i=0; i<hand.size(); i++){
+            int valueOfConstruction = hand.get(i).getValue();
+            if (gold >= valueOfConstruction){
+                gold -= valueOfConstruction;
+                city.add(hand.get(i));
+                hand.remove(i);
+                return;
+            }
+        }
+    }
+
+    public String toString(){
+        return "Player " + this.getNumber();
+    }
 }
+
---
+++ src/test/java/fr/cotedazur/univ/polytech/startingpoint/PlayerTest.java
@@ -0,0 +1,96 @@
+package fr.cotedazur.univ.polytech.startingpoint;
+
+import fr.cotedazur.univ.polytech.startingpoint.cards.Color;
+import fr.cotedazur.univ.polytech.startingpoint.cards.Constructions;
+import fr.cotedazur.univ.polytech.startingpoint.players.City;
+import fr.cotedazur.univ.polytech.startingpoint.players.Hand;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+
+    Hand hand1;
+    Hand hand2;
+    Player p1;
+    Player p2;
+    Draw draw;
+
+    void init() {
+        hand1 = new Hand();
+
+        p1 = new Player(1, hand1);
+        p1.getHand().add(new Constructions("Temple", Color.RELIGIEUX, 1));
+        p1.getHand().add(new Constructions("Forteresse", Color.SOLDATESQUE, 2));
+
+        draw = new Draw();
+        draw.addXConstructions(new Constructions("Cathédrale", Color.RELIGIEUX, 5), 1);
+        draw.addXConstructions(new Constructions("Château", Color.NOBLE, 4), 1);
+        draw.addXConstructions(new Constructions("Monastère", Color.RELIGIEUX, 3), 1);
+        draw.addXConstructions(new Constructions("Marché", Color.COMMERCIAL, 2), 1);
+        draw.addXConstructions(new Constructions("Comptoir", Color.COMMERCIAL, 3), 1);
+
+        hand2 = new Hand();
+
+        p2 = new Player(2, 1, hand2, new City());
+        p2.getHand().add(new Constructions("Temple", Color.RELIGIEUX, 1));
+        p2.getHand().add(new Constructions("Forteresse", Color.SOLDATESQUE, 2));
+    }
+
+    @Test
+    void play() {
+        init();
+        assertEquals(1, p2.getGold());
+        p2.play(draw);
+        assertEquals(2, p2.getGold());
+        assertEquals("Temple", p2.getCity().get(0).getName());
+        assertEquals("Forteresse", p2.getHand().get(0).getName());
+        p2.play(draw);
+        assertEquals(2, p2.getGold());
+        assertEquals("Forteresse", p2.getCity().get(1).getName());
+        assertTrue(p2.getHand().isEmpty());
+        p2.play(draw);
+        assertEquals(2, p2.getGold());
+        assertEquals("Château", p2.getHand().get(0).getName());
+        p2.play(draw);
+        assertEquals(0, p2.getGold());
+        assertEquals(0, p2.getHand().size());
+        assertEquals(3, p2.getCity().size());
+        p2.play(draw);
+        assertEquals(0, p2.getGold());
+        assertEquals("Marché", p2.getHand().get(0).getName());
+        assertEquals("Château", p2.getCity().get(2).getName());
+        assertEquals(1, p2.getHand().size());
+        assertEquals(3, p2.getCity().size());
+    }
+
+    @Test
+    void takeConstruction() {
+        init();
+        assertEquals("Château", p1.takeConstruction(draw).getName());
+        assertEquals("Marché", p1.takeConstruction(draw).getName());
+        assertEquals("Comptoir", p1.takeConstruction(draw).getName());
+    }
+
+    @Test
+    void takeGold() {
+        init();
+        assertEquals(2, p1.getGold());
+        p1.takeGold();
+        assertEquals(4, p1.getGold());
+    }
+
+    @Test
+    void buildConstruction() {
+        init();
+        p1.buildConstruction();
+        assertEquals(2, p1.getHand().get(0).getValue());
+        assertEquals(1, p1.getHand().size());
+
+        assertEquals(1, p1.getCity().get(0).getValue());
+        assertEquals(1, p1.getCity().size());
+
+        assertEquals(1, p1.getGold());
+    }
+}
