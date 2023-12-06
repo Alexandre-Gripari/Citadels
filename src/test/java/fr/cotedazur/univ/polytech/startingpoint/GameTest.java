217a096ca0177b2fe73676b2fdd25ef5f56fe632
Ilias Moutaoukil <ilias.moutaoukil@etu.unice.fr>
2023-12-06 19:49:29 +0100

    Rectification de bug dans les tests #8

--- src/test/java/fr/cotedazur/univ/polytech/startingpoint/GameTest.java
+++ src/test/java/fr/cotedazur/univ/polytech/startingpoint/GameTest.java
@@ -1,5 +1,6 @@
 package fr.cotedazur.univ.polytech.startingpoint;
 
+import fr.cotedazur.univ.polytech.startingpoint.players.Hand;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,7 +10,7 @@ class GameTest {
     @Test
     void init() {
 
-        Game game = new Game(new Player[]{new Player(), new Player()});
+        Game game = new Game(new Player[]{new Player(1, new Hand()), new Player(2, new Hand())});
         game.init();
         assertEquals(2, game.getPlayers().length);
         assertEquals(54, game.getDraw().size());
